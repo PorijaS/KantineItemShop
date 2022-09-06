319544bf1c068d5059b9773f15c0416e3f1e2f58
Laz <queue.triple@gmail.com>
2022-09-06 09:35:48 +0200

    Installed packages, created dbContext and UserService

---
+++ KantineAPIv2/Context/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using KantineAPIv2.Entities;
+using System.Collections.Generic;
+
+namespace KantineAPIv2.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OrderLine> OrderLines { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<FoodCategory> FoodCategories { get; set; }
+    }
+}
---
+++ KantineAPIv2/Services/CurrentUserService.cs
@@ -0,0 +1,32 @@
+﻿using KantineAPIv2.Entities.DataRepository;
+using System.Security.Claims;
+
+namespace KantineAPIv2.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public long userId
+        {
+            get
+            {
+                string? identifier = _contextAccessor.HttpContext?.User?.FindFirstValue("UserId");
+
+                if (!string.IsNullOrEmpty(identifier))
+                {
+                    return long.Parse(identifier);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
